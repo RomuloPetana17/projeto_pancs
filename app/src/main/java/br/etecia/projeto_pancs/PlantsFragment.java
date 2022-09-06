945ebf2cad8cdc2fa99618cd53172a476eac6c87
RomuloPetana17 <romulohenrique082@gmail.com>
2022-09-06 20:20:50 -0300

    Initial

---
+++ app/src/main/java/br/etecia/projeto_pancs/PlantsFragment.java
@@ -0,0 +1,20 @@
+package br.etecia.projeto_pancs;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class PlantsFragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.fragment_plants, container, false);
+        return view;
+    }
+}
